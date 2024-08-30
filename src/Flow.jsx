8cdfc1c93780d275009b813cc53f83a53c559a65
AVidhanR <itsvidhanreddy@gmail.com>
2024-08-30 13:03:06 +0530

    refactor: Update Flow component to use custom HomeNode type

--- src/Flow.jsx
+++ src/Flow.jsx
@@ -6,36 +6,25 @@ import {
   applyNodeChanges,
   addEdge,
   MiniMap,
+  ReactFlowProvider,
 } from "@xyflow/react";
 import "@xyflow/react/dist/style.css";
 import { useState, useCallback, useEffect } from "react";
 import "./index.css";
-
-const initialEdges = [
-  {
-    id: "1-2",
-    source: "1",
-    target: "2",
-    label: "to",
-    type: "step",
-  },
-];
+import HomeNode from "./customs/HomeNode";
 
 const rfStyle = {
   backgroundColor: "#B8CEFF",
 };
 
+const nodeTypes = { homeNode: HomeNode };
+
 const initialNodes = [
   {
     id: "1",
-    data: { label: "My Initial Node" },
-    position: { x: 250, y: 25 },
-  },
-  {
-    id: "2",
-    data: { label: "My Second Node" },
-    type: "output",
-    position: { x: 100, y: 125 },
+    type: "homeNode",
+    data: { value: "Home Node" },
+    position: { x: 250, y: 250 },
   },
 ];
 
@@ -46,7 +35,7 @@ const getId = () => `node-${id++}`;
 function Flow() {
   // we know these
   const [nodes, setNodes] = useState(initialNodes);
-  const [edges, setEdges] = useState(initialEdges);
+  const [edges, setEdges] = useState([]);
   const [nodeName, setNodeName] = useState("");
   // we know these
   const onNodesChange = useCallback(
@@ -66,22 +55,15 @@ function Flow() {
     []
   );
 
-  // const addNode = () => {
-  //   const newNode = {
-  //     id: getId(),
-  //     data: { label: `Node ${nodes.length + 1}` },
-  //     position: { x: Math.random() * 250, y: Math.random() * 250 },
-  //   };
-  //   setNodes((nds) => [...nds, newNode]);
-  // };
-
-  // Add a new node to the flow chart when the button is clicked and set its position to a random location on the canvas area.
+  // Add a new node to the flow chart when the button is clicked
+  // and set its position to a random location on the canvas area.
   const addNode = useCallback(() => {
     setNodes((nds) => [
       ...nds,
       {
         id: getId(),
-        data: { label: nodeName || "Node" },
+        type: "homeNode",
+        data: { value: nodeName || "Node" },
         position: { x: Math.random() * 250, y: Math.random() * 250 },
       },
     ]);
@@ -89,12 +71,13 @@ function Flow() {
   }, [setNodes, nodeName]);
 
   /**
-   ** Kinda long but please go through this carefully.
-   * Save flow state to session storage on every change to nodes or edges or on initial render only if there is no saved state in session storage yet (e.g. on first visit to the page)
-   * or if the saved state is empty (e.g. if the user has cleared their session storage)
-   * or if the saved state is not empty but the user has cleared their browser cache since the last visit to the page
-   * (e.g. if the user has cleared their browser cache since the last visit to the page)
-   */
+   * Kinda long but please go through this carefully.
+   * Save flow state to session storage on every change to nodes
+   * or edges or on initial render only if there is no saved state in session storage yet
+   * (e.g. on first visit to the page)
+   **/
+
+  // Save flow state to session storage
   const saveFlowState = useCallback(() => {
     sessionStorage.setItem(
       "react-flow-state",
@@ -118,35 +101,35 @@ function Flow() {
 
   // kinda complex but i've got upto here for now
   return (
-    <div style={{ height: "100vh", fontFamily: "Open Sans" }}>
-      {/* <button onClick={addNode} style={{ position: "absolute", zIndex: 15 }}>
-        Add Node
-      </button> */}
-      <div className="custom-node-field">
-        <p>To remove a node, select the node and press backspace.</p>
-        <input
-          type="text"
-          value={nodeName}
-          onChange={(e) => setNodeName(e.target.value)}
-          placeholder="Enter node name"
-        />
-        <button onClick={addNode}>Add Node</button>
-      </div>
+    <ReactFlowProvider>
+      <div style={{ height: "100vh", fontFamily: "Open Sans" }}>
+        <div className="custom-node-field">
+          <p>To remove a node, select the node and press backspace.</p>
+          <input
+            type="text"
+            value={nodeName}
+            onChange={(e) => setNodeName(e.target.value)}
+            placeholder="Enter node name"
+          />
+          <button onClick={addNode}>Add Node</button>
+        </div>
 
-      <ReactFlow
-        nodes={nodes}
-        edges={edges}
-        onNodesChange={onNodesChange}
-        onEdgesChange={onEdgesChange}
-        onConnect={onConnect}
-        style={rfStyle}
-        fitView
-      >
-        <MiniMap />
-        <Background />
-        <Controls />
-      </ReactFlow>
-    </div>
+        <ReactFlow
+          nodes={nodes}
+          edges={edges}
+          onNodesChange={onNodesChange}
+          onEdgesChange={onEdgesChange}
+          onConnect={onConnect}
+          style={rfStyle}
+          nodeTypes={nodeTypes}
+          fitView
+        >
+          <MiniMap />
+          <Background />
+          <Controls />
+        </ReactFlow>
+      </div>
+    </ReactFlowProvider>
   );
 }
 
